f1e69d04a6543827410cc0bba646ebfe6100537b
Erasmo Freitas <erasmo_sf@hotmail.com>
2019-12-20 00:38:09 -0300

    Add project files.

---
+++ ImageUpload/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ImageUpload.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(HttpPostedFileBase file)
+        {
+            if (file != null)
+            {
+                ViewBag.Message = "Imagem Carregada";
+            }
+            else
+            {
+                ViewBag.Message = "Erro no Upload";
+            }
+            return View();
+        }
+
+    }
+}
